042954b3cfe279790399652d06a179a44f6ad856
keerthan0803 <23eg106b48@anurag.edu.in>
2026-03-11 12:07:25 +0530

    first commit

--- src/prediction.py
+++ src/prediction.py
@@ -9,4 +9,4 @@ class Insurance_Prediction:
     def predict(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
         X = [[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]]
         X_scaled = self.scaler.transform(X)
-        return self.model.predict(X_scaled)[0]
+        return self.model.predict(X_scaled)
