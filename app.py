1e1800078c86fbb5b47fdbee49d1a1ea47020ba8
keerthan0803 <23eg106b48@anurag.edu.in>
2026-03-11 12:12:47 +0530

    first commit

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ Sum_Assured_Lakhs=st.number_input("Enter Sum Assured in Lakhs")
 if st.button("Predict"):
     model = Insurance_Prediction()
     prediction = model.predict(Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs)
-    st.success(f"The predicted Annual Premium in Thousands is: {float(prediction[0]):.2f}")
+    st.success(f"The predicted Annual Premium in Thousands is: {float(prediction[0])}")
     st.balloons()
